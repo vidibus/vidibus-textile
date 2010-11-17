b0b2cb44b79b75346a5fee88db3b68af2e08ebea
Andre Pankratz <andre@webwarelab.com>
2010-11-18 00:59:57 +0100

    Configure mongoid for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,18 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 
 require "rubygems"
 require "rspec"
+require "rr"
 require "vidibus-textile"
+
+Mongoid.configure do |config|
+  name = "vidibus-textile_test"
+  host = "localhost"
+  config.master = Mongo::Connection.new.db(name)
+end
 
 RSpec.configure do |config|
   config.mock_with :rr
+  config.after :suite do
+    Mongoid.master.collections.select {|c| c.name !~ /system/ }.each(&:drop)
+  end
 end
